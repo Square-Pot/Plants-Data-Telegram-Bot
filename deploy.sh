fa089bcbf31e50ed8592e987934a2375d87e4a98
demetrius <d.natxa@gmail.com>
2024-11-07 01:32:54 +0300

    test actions

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 
 cd /home/dntx/projects/Plants-Data-Telegram-Bot
 git pull origin main
-sudo systemctl stop plants_data_bot.service
-sudo systemctl start plants_data_bot.service
+sudo systemctl restart plants_data_bot.service
 
 
